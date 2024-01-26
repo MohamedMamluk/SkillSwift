1691791171644c19d5fe881a1dfcfc4f057864cd
MohamedMamluk <mohamedmamlukali@gmail.com>
2024-01-26 12:38:44 +0200

    feat: add initial user schema

---
+++ backend/apps/auth/src/users/models/user.schema.ts
@@ -0,0 +1,36 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import * as bcrypt from 'bcrypt';
+@Schema({ timestamps: true })
+export class User {
+  @Prop({ default: null })
+  image: string;
+
+  @Prop({ required: true })
+  firstName: string;
+
+  @Prop({ required: true })
+  lastName: string;
+
+  @Prop({ default: [] })
+  achievements: string[];
+
+  @Prop({ required: true })
+  email: string;
+
+  @Prop({ required: true })
+  password: string;
+
+  @Prop({ required: true })
+  phoneNumber: string;
+}
+
+export const userDocument = SchemaFactory.createForClass(User);
+
+userDocument.pre('save', async function () {
+  if (!this.isNew) {
+    return;
+  }
+  const hashedPassword = await bcrypt.hash(this.password, 12);
+
+  this.password = hashedPassword;
+});
