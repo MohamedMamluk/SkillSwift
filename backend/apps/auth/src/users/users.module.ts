421f55cd1132da7dc5fbe644dbfca5a529629a3b
MohamedMamluk <mohamedmamlukali@gmail.com>
2024-01-26 15:27:16 +0200

    feat: add necessary providers

--- backend/apps/auth/src/users/users.module.ts
+++ backend/apps/auth/src/users/users.module.ts
@@ -1,4 +1,16 @@
+import { DatabaseModule } from '@app/common';
 import { Module } from '@nestjs/common';
+import { User, userSchema } from './models/user.schema';
+import { UserRepository } from './users.repository';
+import { UserService } from './users.service';
+import { UserController } from './users.controller';
 
-@Module({})
+@Module({
+  imports: [
+    DatabaseModule,
+    DatabaseModule.forFeature([{ name: User.name, schema: userSchema }]),
+  ],
+  providers: [UserRepository, UserService],
+  controllers: [UserController],
+})
 export class UsersModule {}
