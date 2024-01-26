2396643c46a52bbb119ff26a67fc72bbc5f2bc9c
MohamedMamluk <mohamedmamlukali@gmail.com>
2024-01-26 18:34:09 +0200

    feat: Add necessary modules and services for auth

--- backend/apps/auth/src/auth.module.ts
+++ backend/apps/auth/src/auth.module.ts
@@ -1,21 +1,37 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
+import { JwtModule } from '@nestjs/jwt';
 import * as Joi from 'joi';
+import { LocalStrategy } from './strategies/local.strategy';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
+      isGlobal: true,
       validationSchema: Joi.object({
         MONGO_URI: Joi.string().required(),
         PORT: Joi.string().required(),
+        JWT_SECRET: Joi.string().required(),
+        JWT_EXPIRES: Joi.number().required(),
       }),
+    }),
+    JwtModule.registerAsync({
+      useFactory: (configService: ConfigService) => {
+        return {
+          secret: configService.get('JWT_SECRET'),
+          signOptions: {
+            expiresIn: configService.get('JWT_EXPIRES') + 's', //expiration in seconds
+          },
+        };
+      },
+      inject: [ConfigService],
     }),
     UsersModule,
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, LocalStrategy],
 })
 export class AuthModule {}
