93c88009c482f5c42514e9bd31b03552925f859d
MohamedMamluk <mohamedmamlukali@gmail.com>
2024-02-03 15:04:02 +0200

    chore: Add some course methods

--- backend/apps/courses/src/courses.repository.ts
+++ backend/apps/courses/src/courses.repository.ts
@@ -1,14 +1,17 @@
 import { AbstractRepository } from '@app/common';
 import { Injectable, Logger } from '@nestjs/common';
 import { Course } from './schemas/course.schema';
-import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import { InjectConnection, InjectModel } from '@nestjs/mongoose';
+import { Connection, Model } from 'mongoose';
 
 @Injectable()
 export class CoursesRepository extends AbstractRepository<Course> {
   protected readonly logger = new Logger(CoursesRepository.name);
 
-  constructor(@InjectModel(Course.name) CourseModel: Model<Course>) {
-    super(CourseModel);
+  constructor(
+    @InjectModel(Course.name) CourseModel: Model<Course>,
+    @InjectConnection() connection: Connection,
+  ) {
+    super(CourseModel, connection);
   }
 }
--- backend/apps/courses/src/courses.service.ts
+++ backend/apps/courses/src/courses.service.ts
@@ -2,12 +2,13 @@ import { BadRequestException, Injectable } from '@nestjs/common';
 import { CoursesRepository } from './courses.repository';
 import { CourseDto } from './dto/course.dto';
 import mongoose from 'mongoose';
+import { User } from '@app/common';
 
 @Injectable()
 export class CoursesService {
   constructor(private readonly coursesRepo: CoursesRepository) {}
-  async create(courseDetails: CourseDto) {
-    return this.coursesRepo.create(courseDetails);
+  async create(courseDetails: CourseDto, user: User) {
+    return this.coursesRepo.create({ ...courseDetails, seller: user._id });
   }
   async findOne(courseId: string) {
     if (!mongoose.Types.ObjectId.isValid(courseId)) {
