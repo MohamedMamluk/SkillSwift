2121ce1b40de758011b66034de6fe94458499b2a
MohamedMamluk <mohamedmamlukali@gmail.com>
2024-02-03 14:59:59 +0200

    feat: Switch auth to be a hybrid service

--- backend/apps/auth/src/main.ts
+++ backend/apps/auth/src/main.ts
@@ -2,12 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { AuthModule } from './auth.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { Queues, RmqService } from '@app/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AuthModule);
+  const configService = app.get(ConfigService);
+  const rmqService = app.get(RmqService);
+  app.connectMicroservice(rmqService.getOptions(Queues.AUTH, true));
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
-  const configService = app.get(ConfigService);
   await app.listen(configService.get('PORT'));
+  await app.startAllMicroservices();
 }
 bootstrap();
