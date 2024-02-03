ec885c40d9ecaeb38019dd81424a975af170b365
MohamedMamluk <mohamedmamlukali@gmail.com>
2024-02-03 15:02:13 +0200

    feat: Switch courses to be a hybrid service

--- backend/apps/courses/src/main.ts
+++ backend/apps/courses/src/main.ts
@@ -2,12 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { CoursesModule } from './courses.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { Queues, RmqService } from '@app/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(CoursesModule);
   app.useGlobalPipes(new ValidationPipe());
-
   const configService = app.get(ConfigService);
+  const rmqService = app.get(RmqService);
+  app.connectMicroservice(rmqService.getOptions(Queues.COURSES, true));
+  app.startAllMicroservices();
+
   await app.listen(configService.get('PORT'));
 }
 bootstrap();
