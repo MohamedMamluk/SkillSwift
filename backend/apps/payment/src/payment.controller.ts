fac654a1ce83c14c0eb3694e9029060b3c9a687d
MohamedMamluk <mohamedmamlukali@gmail.com>
2024-02-03 15:05:18 +0200

    feat: Add payment microservice

--- backend/apps/payment/src/payment.controller.ts
+++ backend/apps/payment/src/payment.controller.ts
@@ -1,5 +1,8 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { PaymentService } from './payment.service';
+import { MessagePattern, Payload } from '@nestjs/microservices';
+import { CreateChargeDto } from '@app/common';
+import { JwtAuthGuard } from '@app/common';
 
 @Controller()
 export class PaymentController {
@@ -8,5 +11,17 @@ export class PaymentController {
   @Get()
   getHello(): string {
     return this.paymentService.getHello();
+  }
+  @UseGuards(JwtAuthGuard)
+  @MessagePattern('initiate_payment')
+  async initiatePayment(@Payload() createCharge: CreateChargeDto) {
+    console.log('called', { createCharge });
+    return this.paymentService.initiatePayment(createCharge);
+  }
+  @UseGuards(JwtAuthGuard)
+  @MessagePattern('abort_payment')
+  async abortPayment(@Payload() paymentDetails: { paymentId: string }) {
+    console.log(paymentDetails);
+    return this.paymentService.abortPayment(paymentDetails);
   }
 }
--- backend/apps/payment/src/payment.module.ts
+++ backend/apps/payment/src/payment.module.ts
@@ -1,21 +1,26 @@
 import { Module } from '@nestjs/common';
 import { PaymentController } from './payment.controller';
 import { PaymentService } from './payment.service';
-import { DatabaseModule } from '@app/common';
 import * as Joi from 'joi';
 import { ConfigModule } from '@nestjs/config';
+import { RmqModule } from '@app/common';
+import { StripeModule } from './stripe/stripe.module';
+import { AuthModule } from '@app/common';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
+      isGlobal: true,
       validationSchema: Joi.object({
-        MONGO_URI: Joi.string().required(),
-        PORT: Joi.string().required(),
+        RABBIT_MQ_URI: Joi.string().required(),
+        RABBIT_MQ_PAYMENT_QUEUE: Joi.string().required(),
+        RABBIT_MQ_AUTH_QUEUE: Joi.string().required(),
+        STRIPE_SECRET_KEY: Joi.string().required(),
       }),
     }),
-
-    DatabaseModule,
-    DatabaseModule.forFeature([]),
+    RmqModule,
+    StripeModule,
+    AuthModule,
   ],
   controllers: [PaymentController],
   providers: [PaymentService],
