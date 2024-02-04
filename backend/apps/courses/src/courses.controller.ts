296ccee93a0a1e6a6f98d9833d60d34ed6bc5b4a
MohamedMamluk <mohamedmamlukali@gmail.com>
2024-02-04 12:28:51 +0200

    chore: merge 'findCourseRMQ' to 'findCourse'

--- backend/apps/courses/src/courses.controller.ts
+++ backend/apps/courses/src/courses.controller.ts
@@ -17,19 +17,13 @@ export class CoursesController {
     return this.coursesService.create(courseDetails, user);
   }
   @Get(':id')
+  @MessagePattern('get_course')
   async findCourse(
     @Payload() payload: { _id: string },
     @Param('id') courseId: string,
   ) {
     return this.coursesService.findOne(courseId || payload._id);
   }
-  @MessagePattern('get_course')
-  async findCourseRMQ(@Payload() payload: { _id: string }) {
-    const course = await this.coursesService.findOne(payload._id);
-    console.log(course);
-    return this.coursesService.findOne(payload._id);
-  }
-
   @Get()
   async findAll() {
     return this.coursesService.findAll();
