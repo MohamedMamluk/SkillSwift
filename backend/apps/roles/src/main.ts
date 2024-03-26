c4d430a8ead8406850699a8f3f3c40bd3ad402f3
MohamedMamluk <mohamedmamlukali@gmail.com>
2024-03-26 12:59:23 +0200

    chore: Update module to be a hybrid application

--- backend/apps/roles/src/main.ts
+++ backend/apps/roles/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { RolesModule } from './roles.module';
+import { Queues, RmqService } from '@app/common';
+import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(RolesModule);
-  await app.listen(3000);
+  app.useGlobalPipes(new ValidationPipe());
+  const configService = app.get(ConfigService);
+  const rmqService = app.get(RmqService);
+  app.connectMicroservice(rmqService.getOptions(Queues.ROLES, true));
+  app.startAllMicroservices();
+
+  await app.listen(configService.get('PORT'));
 }
 bootstrap();
--- backend/apps/roles/src/roles.module.ts
+++ backend/apps/roles/src/roles.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
+import { AuthModule, DatabaseModule, RmqModule } from '@app/common';
+
+import { ConfigModule } from '@nestjs/config';
+
+import * as Joi from 'joi';
+
 import { RolesController } from './roles.controller';
 import { RolesService } from './roles.service';
 
 @Module({
-  imports: [],
+  imports: [
+    DatabaseModule,
+    // DatabaseModule.forFeature([{ schema: CourseSchema, name: Course.name }]),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        MONGO_URI: Joi.string().required(),
+        PORT: Joi.string().required(),
+        RABBIT_MQ_URI: Joi.string().required(),
+        RABBIT_MQ_AUTH_QUEUE: Joi.string().required(),
+      }),
+    }),
+    RmqModule,
+    AuthModule,
+  ],
   controllers: [RolesController],
   providers: [RolesService],
 })
